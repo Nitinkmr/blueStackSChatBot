da817baab1567255ec9195ffaa2a452cdcd778b0
Nitin Kumar <nitin5.kumar@airtelbank.com>
2019-10-23 18:35:35 +0530

    deployment

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ require('./config/DBConfig');
 require('./index');
 var app = express();
 var listener = app.listen(process.env.PORT || 8080, function(){
-   console.log('Listening on port ' + listener.address().port); 
+   console.log('Listening on port number ' + listener.address().port); 
 });
