502f7863c971329e66e0d70c30534bcabbd792c1
Nitin Kumar <nitin5.kumar@airtelbank.com>
2019-10-23 19:09:32 +0530

    deployment changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const discord = require('discord.js');
+const discord = require('discord');
 
 const client = new discord.Client();
 const properties = require('./properties/application.properties');
